9a657190866bfa5005eb1f65e67e1fb256551e20
kelsoncm <kelsoncm@gmail.com>
2021-11-03 11:51:21 -0300

    [UPD] version

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'auth_suap';
 $plugin->release = '0.1.0';
-$plugin->version = 2021110215;
+$plugin->version = 2021110316;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_ALPHA;
