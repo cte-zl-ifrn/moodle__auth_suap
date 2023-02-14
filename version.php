5812de6b4a4ef632ce7e1232dde9673302f317ef
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2023-02-14 14:48:36 -0300

    release: 019

--- version.php
+++ version.php
@@ -10,7 +10,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'auth_suap';
-$plugin->release = '0.2.024';
+$plugin->release = '0.2.019';
 $plugin->version = 20230210000 + substr($plugin->release, 4);
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->requires = 2019052000;
