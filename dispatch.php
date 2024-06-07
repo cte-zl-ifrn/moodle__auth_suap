f68383806e038c9ddf890d2026bab7f418ddb468
HilquiasAbias <hilquias.dev@gmail.com>
2024-06-07 15:42:59 -0300

    fix: autenticação por despachante.

--- dispatch.php
+++ dispatch.php
@@ -1,21 +1,33 @@
 <?php
+
 define('AJAX_SCRIPT', true);
 define('REQUIRE_CORRECT_ACCESS', true);
 define('NO_MOODLE_COOKIES', true);
 
 require_once(__DIR__ . '/../../config.php');
+require_once(__DIR__ . '/../../lib/externallib.php');
+
+// Permições de CORS para requisições PREFLIGHT (ionic)
+if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authentication");
+    exit;
+}
 
 // Allow CORS requests.
 header('Access-Control-Allow-Origin: *');
 
 function validate_enabled_web_services() {
+    global $DB, $CFG, $SERVICE;
+
     if (!$CFG->enablewebservices) {
         throw new moodle_exception('enablewsdescription', 'webservice');
     }
 
     // Não pode se o serviço não existir e não estiver habilitado
-    $service = $DB->get_record('external_services', array('shortname' => $_GET['service'], 'enabled' => 1));
-    if (empty($service)) {
+    $SERVICE = $DB->get_record('external_services', array('shortname' => $_GET['service'], 'enabled' => 1));
+    if (empty($SERVICE)) {
         throw new moodle_exception('servicenotavailable', 'webservice');
     }
 
@@ -29,22 +41,29 @@ function validate_enabled_web_services() {
 
 function authenticate_service_caller() {
     $headers = getallheaders();
+
+    // Verifica se o token de autenticação está no header
     $authentication_key = array_key_exists('Authentication', $headers) ? "Authentication": "authentication";
     if (!array_key_exists($authentication_key, $headers)) {
         throw new \Exception("Bad Request - Authentication not informed", 400);
     }
 
-    // Verifica se o token de autenticação está no header
-    $sync_up_auth_token = config('auth_token');
-    if ("Token $sync_up_auth_token" != $headers[$authentication_key]) {
-        throw new \Exception("Unauthorized", 401);
-    }
-
+    // Valida token de autenticação
+    // $sync_up_auth_token = config('auth_token');
+    // if ("Token $sync_up_auth_token" != $headers[$authentication_key]) {
+    //     throw new \Exception("Unauthorized", 401);
+    // }
+    // TODO: Implementar validação do token
 }
 
 function authenticate_user() {
-    global $USER;
-    $username = $_GET['username']
+    global $USER, $DB;
+
+    if (isset($_GET['username'])) {
+        $username = $_GET['username'];
+    } else {
+        throw new moodle_exception('missingusername', 'webservice');
+    }
 
     // echo $OUTPUT->header();
 
@@ -84,24 +103,35 @@ function authorize_user() {
     }
 
     // let enrol plugins deal with new enrolments if necessary
-    enrol_check_plugins($user);
+    enrol_check_plugins($USER);
 
     // setup user session to check capability
-    \core\session\manager::set_user($user);
+    \core\session\manager::set_user($USER);
 
     $USER->site_admin = has_capability('moodle/site:config', $systemcontext, $USER->id);
 }
 
 function response_token() {
-    // Get an existing token or create a new one.
-    $token = \core_external\util::generate_token_for_current_user($service);
+    global $SERVICE;
+
+    $token = external_generate_token_for_current_user($SERVICE);
+
+    // prod
+    // echo json_encode(
+    //     [
+    //         "token" => $token->token,
+    //         "privatetoken" => is_https() && !$USER->site_admin ? $token->privatetoken : null,
+    //     ]
+    // );
+
+    // dev
     echo json_encode(
         [
             "token" => $token->token,
-            "privatetoken" => is_https() && !$USER->site_admin ? $token->privatetoken : null,
+            "privatetoken" => !$USER->site_admin ? $token->privatetoken : null,
         ]
     );
-    \core_external\util::log_token_request($token);
+    external_log_token_request($token);
 }
 
 validate_enabled_web_services();
